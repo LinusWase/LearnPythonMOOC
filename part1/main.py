31119720d6c1a396eeae654b725d8d4072689741
linuswase <linusmwase@gmail.com>
2025-09-01 15:17:19 +0200

    Started learning python

---
+++ part1/main.py
@@ -0,0 +1,8 @@
+print("Hi there")
+
+print("Hours in a year:")
+#there are 365 days in a year and 24 hours in each day
+print(365*24)
+
+#can be printed like this. Comes in handy when you want to print quotation marks
+print('print("Hello there!")')
