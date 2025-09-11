f5e1ec2a899f2dbdcb528c1d2bafea0696a6410c
LinusWase <linusmwase@gmail.com>
2025-09-11 18:23:42 +0200

    added more exercises

---
+++ Part4/definite_iteration/main.py
@@ -0,0 +1,26 @@
+my_list = [3, 2, 4, 5, 2]
+
+index = 0
+while index < len(my_list):
+    print(my_list[index])
+    index += 1
+
+print("For loop:")
+for item in my_list:
+    print(item)
+
+name = input("Please type in your name: ")
+for character in name:
+    print(character)
+
+for i in range(5):
+    print(i)
+
+for i in range(2,6):
+    print(i)
+
+for i in range(10,20,2):
+    print(i)
+
+for i in range(6,2,-2):
+    print(i)
---
+++ Part4/definite_iteration/star_sudded.py
@@ -0,0 +1,26 @@
+"""
+Please write a program which asks the user to type in a string. The program then prints each input character on a
+separate line. After each character there should be a star (*) printed on its own line.
+
+This is how it should work:
+Sample output
+
+Please type in a string: Python
+P
+*
+y
+*
+t
+*
+h
+*
+o
+*
+n
+*
+"""
+
+user_input = input("Please type in a string: ")
+for character in user_input:
+    print(character)
+    print("*")
