77cccda9a5d9184d0a2f7633cc11d3cd9c4cc561
linuswase <linusmwase@gmail.com>
2025-09-11 15:44:28 +0200

    added exercise

---
+++ Part4/lists/main.py
@@ -0,0 +1,30 @@
+my_list = [7, 2, 2, 5, 2]
+
+print(my_list[0])
+print(my_list[1])
+print(my_list[3])
+
+print("The sum of the first two items:", my_list[0] + my_list[1])
+
+print(my_list)
+my_list[2] = 5
+print(my_list)
+print(len(my_list))
+
+numbers = []
+shoe_sizes = []
+
+numbers.append(5)
+numbers.append(10)
+numbers.append(3)
+
+shoe_sizes.append(37)
+shoe_sizes.append(44)
+shoe_sizes.append(40)
+shoe_sizes.append(28)
+
+print("Numbers:")
+print(numbers)
+
+print("Shoe sizes:")
+print(shoe_sizes)
