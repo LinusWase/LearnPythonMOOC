2b304b6b8e78bbae8a6bb8a4284ebc3c38f026bb
LinusWase <linusmwase@gmail.com>
2025-09-17 18:06:51 +0200

    added more exercises

--- Part5/references/sudoku_add_number_to_a_copy_of_the_grid.py
+++ Part5/references/sudoku_add_number_to_a_copy_of_the_grid.py
@@ -1,13 +1,11 @@
 """
-In this exercise we will complete two more functions for the sudoku project from the previous section: print_sudoku and
-add_number.
+This is the very last sudoku task. This time we will create a slightly different version of the function for adding new
+numbers to the grid.
 
-The function print_sudoku(sudoku: list) takes a two-dimensional array representing a sudoku grid as its argument. The
-function should print out the grid in the format specified in the examples below.
-
-The function add_number(sudoku: list, row_no: int, column_no: int, number:int) takes a two-dimensional array
+The function copy_and_add(sudoku: list, row_no: int, column_no: int, number: int) takes a two-dimensional array
 representing a sudoku grid, two integers referring to the row and column indexes of a single square, and a single digit
-between 1 and 9, as its arguments. The function should add the digit to the specified location in the grid.
+
+The print_sudoku function from the previous exercise could be useful for testing, and it is used in the example below:
 
 sudoku  = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -21,17 +19,16 @@ sudoku  = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
+grid_copy = copy_and_add(sudoku, 0, 0, 2)
+print("Original:")
 print_sudoku(sudoku)
-add_number(sudoku, 0, 0, 2)
-add_number(sudoku, 1, 2, 7)
-add_number(sudoku, 5, 7, 3)
 print()
-print("Three numbers added:")
-print()
-print_sudoku(sudoku)
+print("Copy:")
+print_sudoku(grid_copy)
 
 Sample output
 
+Original:
 _ _ _  _ _ _  _ _ _
 _ _ _  _ _ _  _ _ _
 _ _ _  _ _ _  _ _ _
@@ -44,36 +41,24 @@ _ _ _  _ _ _  _ _ _
 _ _ _  _ _ _  _ _ _
 _ _ _  _ _ _  _ _ _
 
-Three numbers added:
-
+Copy:
 2 _ _  _ _ _  _ _ _
-_ _ 7  _ _ _  _ _ _
-_ _ _  _ _ _  _ _ _
-
 _ _ _  _ _ _  _ _ _
 _ _ _  _ _ _  _ _ _
-_ _ _  _ _ _  _ 3 _
 
 _ _ _  _ _ _  _ _ _
 _ _ _  _ _ _  _ _ _
 _ _ _  _ _ _  _ _ _
 
-Hint
+_ _ _  _ _ _  _ _ _
+_ _ _  _ _ _  _ _ _
+_ _ _  _ _ _  _ _ _
 
-Remember it is possible to call the print function without causing a line change:
-
-print("characters ", end="")
-print("without carriage returns", end="")
-
-Sample output
-
-characters without carriage returns
-
-Sometimes you need just a new line, which a print statement without any argument will achieve:
-
-print()
+Hint When dealing with nested lists you should be extra careful when copying. What all needs to be explicitly copied,
+and where do changes actually have an effect? The visualisation tool is a great help here, too, although the size of
+the sudoku grid will make the view less orderly than usual.
 """
-
+#TESTING:
 def print_sudoku(sudoku: list):
     outside_counter = 0
     for item in sudoku:
@@ -94,13 +79,14 @@ def print_sudoku(sudoku: list):
         if outside_counter % 3 == 0:
             print()
 
+def copy_and_add(sudoku: list, row_no: int, column_no: int, number: int):
+    new_sudoku = []
+    for row in sudoku:
+        new_row = row[:]
+        new_sudoku.append(new_row)
 
-
-
-
-def add_number(sudoku: list, row_no: int, column_no: int, number:int):
-   sudoku[row_no][column_no] = number
-
+    new_sudoku[row_no][column_no] = number
+    return new_sudoku
 
 if __name__ == '__main__':
     sudoku = [
@@ -115,11 +101,9 @@ if __name__ == '__main__':
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
 
+    grid_copy = copy_and_add(sudoku, 0, 0, 2)
+    print("Original:")
     print_sudoku(sudoku)
-    add_number(sudoku, 0, 0, 2)
-    add_number(sudoku, 1, 2, 7)
-    add_number(sudoku, 5, 7, 3)
     print()
-    print("Three numbers added:")
-    print()
-    print_sudoku(sudoku)
+    print("Copy:")
+    print_sudoku(grid_copy)
